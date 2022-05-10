19765fdd6699760a21b7479f05bfa0fb01caacac
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2022-05-10 15:14:00 -0500

    Add subpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,10 @@ const routes: Routes = [
     path: 'tutorial',
     loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
     canLoad: [CheckTutorial]
+  },
+  {
+    path: 'subpage',
+    loadChildren: () => import('./pages/subpage/subpage.module').then( m => m.SubpagePageModule)
   }
 ];
 
